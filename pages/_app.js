d44cdf69cd4241bb9bfb6c3f37ddee9e3f3baf8a
alexmochu <mochualex4@gmail.com>
2020-01-08 09:12:38 +0300

    Build your user interface with Polaris

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react';
+import '@shopify/polaris/styles.css';
+import translations from '@shopify/polaris/locales/en.json';
+import Cookies from 'js-cookie';
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    const config = { apiKey: API_KEY, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <Provider config={config}>
+        <AppProvider i18n={translations}>
+          <Component {...pageProps} />
+        </AppProvider>
+        </Provider>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyApp;
