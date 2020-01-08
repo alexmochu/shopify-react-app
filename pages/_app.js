e619439f43f6d6c176ef748ad679f3b720308f5a
alexmochu <mochualex4@gmail.com>
2020-01-08 10:32:24 +0300

    Fetch data with Apollo

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,14 @@ import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
 import Cookies from 'js-cookie';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  fetchOptions: {
+    credentials: 'include'
+  },
+});
 
 class MyApp extends App {
   render() {
@@ -18,7 +26,9 @@ class MyApp extends App {
         </Head>
         <Provider config={config}>
         <AppProvider i18n={translations}>
-          <Component {...pageProps} />
+        <ApolloProvider client={client}>
+            <Component {...pageProps} />
+          </ApolloProvider>
         </AppProvider>
         </Provider>
       </React.Fragment>
